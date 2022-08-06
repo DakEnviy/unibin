90be52942ce2a4f55099bcf5915e677dbe275a9e
Mikhail Golbakh <golbah@yandex.ru>
2022-08-06 13:34:38 +0300

    feat: add max file size option

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import { createFile } from './createFile';
 
 const PORT = Number(process.env.PORT) || 9999;
 const MAX_CONNECTIONS = Number(process.env.MAX_CONNECTIONS) || 64;
-const SOCKET_TIMEOUT = Number(process.env.SOCKET_TIMEOUT) || 3000;
+const SOCKET_TIMEOUT = Number(process.env.SOCKET_TIMEOUT) || 3000; // 3 seconds
+const MAX_FILE_SIZE = Number(process.env.MAX_FILE_SIZE) || 10 * 1024 * 1024; // 10 MB
 
 const server = net.createServer();
 
@@ -14,9 +15,22 @@ server.on('connection', socket => {
     socket.setTimeout(SOCKET_TIMEOUT);
 
     const fileParts: Buffer[] = [];
+    let fileSize = 0;
+
+    const appendFilePart = (part: Buffer) => {
+        fileSize += part.byteLength;
+
+        if (fileSize > MAX_FILE_SIZE) {
+            socket.end(`File size should be less than ${MAX_FILE_SIZE} bytes\b`);
+
+            return;
+        }
+
+        fileParts.push(part);
+    };
 
     socket.on('data', data => {
-        fileParts.push(data);
+        appendFilePart(data);
     });
 
     socket.on('timeout', async () => {
